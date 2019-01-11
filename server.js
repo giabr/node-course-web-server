fcf68ae72421f3ba7ac5a9d2c078b29e403730fe
giabr <31702129+giabr@users.noreply.github.com>
2019-01-11 09:07:47 +0700

    initial commit

---
+++ server.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const hbs = require('hbs')
+const fs = require('fs')
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('view engine','hbs');
+
+app.use((req,res,next)=>{
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`
+
+    console.log(log)
+    fs.appendFile('server.log', log + '\n', (err)=>{
+        if(err){
+            console.log('Unable to append to server.log')
+        }
+    })
+    next();
+})
+
+// app.use((req,res,next)=>{
+//     res.render('maintenance.hbs')
+// })
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', ()=>{
+    return new Date().getFullYear()
+})
+
+hbs.registerHelper('screamIt', (text)=>{
+    return text.toUpperCase()
+})
+
+app.get('/', (req, res)=>{
+    // res.send('<h1>Hello World</h1>');
+    // res.send({
+    //     name: 'Joni',
+    //     age: '17',
+    //     hobby: [
+    //         'basketball',
+    //         'sing a song'
+    //     ]
+    // })
+    res.render('index.hbs',{
+        pageTitle: 'Home Page',
+        welcomeMessage: 'Welcome, my dear'
+    })
+})
+
+app.get('/about', (req, res)=>{
+    res.render('about.hbs', {
+        pageTitle: 'About You',
+    });
+})
+
+// /bad
+app.get('/bad', (req,res)=>{
+    res.send({
+        errorMessage: 'Unable to connect to server'
+    })
+})
+
+app.listen(3000, ()=>{
+    console.log('Server is running on port 3000')
+})
